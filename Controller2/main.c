3342e2d4b1b3b6d33fabe8b8c8ad87762e8f9971
Ahmed F. El-Adawy <AhmedFathi77@users.noreply.github.com>
2018-04-16 04:52:53 +0200

    Update main.c

--- Controller2/main.c
+++ Controller2/main.c
@@ -5,11 +5,22 @@
  *      Author: leenovoz510
  */
 
+/*
+ = \												 					  / =
+ =	\												 					 /  =
+ =   \																	/	=
+ =	  >> This Is Controller  ***** => 2 <= ***** This Is Controller    << 	=
+ =   /																    \	=
+ =  /																	 \	=
+ = /																	  \	=
+*/
+
 #include <avr/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "LCD.h"
 #include "interrupt.h"
+#include "UART.h"
 
 volatile int Number_overflow = 0;
 ISR(INT0_vect){
@@ -23,16 +34,27 @@ int main(void){
 	PORTB|=(1<<PB1);
 	PORTB|=(1<<PB0);
 	SREG  |= (1<<7);
+	char str;
 	INT0_init();
 	LCD_Init();
 	_delay_ms(10);
 	LCD_Clear();
 	//Timer0_init_Overflow_mode();
 
+	UART_Init();
 	while(1){
 		Go_to(1 , 1);
 		LCD_IntegerToStringPrint(Number_overflow);
 		Go_to(4 , 1);
 		LCD_Print(" Humans ");
+		str = UART_Receive();
+		if(str == 'F'){
+			Go_to(1 , 2);
+			LCD_Print("Warning FIRE");
+		}else if(str == 'N'){
+			Go_to(1 , 2);
+			LCD_Print("            ");
+		}
+
 	}
 }
