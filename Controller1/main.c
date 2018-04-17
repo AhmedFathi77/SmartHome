0bdfbbc49ea80b0cd56be722bec5039627c889cc
Ahmed F. El-Adawy <AhmedFathi77@users.noreply.github.com>
2018-04-17 06:12:43 +0200

    Update main.c

--- Controller1/main.c
+++ Controller1/main.c
@@ -28,17 +28,9 @@
 
 /* 		------> this Interrupt Works when PIR Detect a human in the house <------ 		*/
 ISR(INT0_vect){
-	Go_to(-3 , 3);
-	LCD_Print("Human Detected");
-	_delay_ms(100);
-	Go_to(-3 , 3);
-	LCD_Print("                ");
-
+	Go_to(-3 , 4);
+	LCD_Print("Do not smoke");
 }
-ISR(TIMER0_OVF_vect){
-
-}
-
 int main(void){
 		INT0_init();
 		Keypad_init();
@@ -47,11 +39,40 @@ int main(void){
 		LCD_Init();
 		_delay_ms(10);
 		LCD_Clear();
-
+		INT0_init();
 		ADC_Init();
 		UART_Init();
 		int Key[4]= {11} , i=0 ;
+		int OpenDoors = 0 , DoorsAreOpen = 1;
+
+		while(DoorsAreOpen){
+				Go_to(1 , 1);
+				LCD_Print("Enter Password");
+				Key[i] = Key_Pressed();
+				if(Key[i] != 11 && i<4){
+						Go_to(1+i ,2);
+						LCD_Print("*");
+						i++;
+					}
+				if(i == 4){
+					if(Key[0] == 1 && Key[1] == 9 && Key[2] ==9 && Key[3] ==6){
+						Go_to(-3 , 3);
+						LCD_Print("Correct Password");
+						_delay_ms(50);
+						OpenDoors = 1;
+						DoorsAreOpen = 0;
+						LCD_Clear();
+					}else{
+						Go_to(-3 , 3);
+						LCD_Print("Wrong Password");
+						i = 0;
+						OpenDoors = 0;
+						LCD_Clear();
+					}
+				}
+			}
 			while(1){
+
 				Go_to(3 , 1);
 				LCD_Print("Welcome Home");
 				Go_to(1 , 2);
@@ -59,7 +80,7 @@ int main(void){
 				Go_to(11 , 2);
 				ADCvalue = ADC_Value() ;
 				LCD_IntegerToStringPrint(ADCvalue);
-
+				_delay_ms(200);
 				if(ADCH > 30){
 					UART_Transmiter('F');
 					Go_to(-2 , 3);
@@ -68,32 +89,7 @@ int main(void){
 					UART_Transmiter('N');
 					Go_to(-2 , 3);
 					LCD_Print("             ");
-								}
-				Go_to(0 , 3);
-				Key[i] = Key_Pressed();
-				if(Key[i] != 11 && i<4){
-							Go_to(-3 , 3);
-							LCD_Print("Enter Password");
-							Go_to(0+i ,4);
-							LCD_Print("*");
-							i++;
-						}
-						if(i == 4){
-							if(Key[0] == 1 && Key[1] == 9 && Key[2] ==9 && Key[3] ==6){
-								Go_to(-2 , 3);
-								LCD_Print("Correct Password");
-								_delay_ms(50);
-								Go_to(0 , 4);
-								LCD_Print("         ");
-							}else{
-
-								Go_to(-2 , 3);
-								LCD_Print("Wrong Password");
-								Go_to(0 , 4);
-								LCD_Print("         ");
-								i = 0;
-							}
-						}
+				}
 		}
 
 }
